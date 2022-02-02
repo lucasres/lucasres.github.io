7643b72284c193445890dcf03b92202ff14fd519
lucasres <lucasresone@gmail.com>
2022-02-01 23:09:24 -0300

    add welcome section

---
+++ index.js
@@ -0,0 +1,8 @@
+// let scrollAtOpensource = false
+
+// document.addEventListener("scroll", function(event) { 
+//     if (window.scrollY < 100 && ! scrollAtOpensource) {
+//         scrollAtOpensource = true
+//         document.getElementById("opensource").scrollIntoView();
+//     }
+// });
